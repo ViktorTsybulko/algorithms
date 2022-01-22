da539c8f0bef52eb4c8a9636a70225ef1811f91c
Viktor Tsybulko <viktortsybulko2002@gmail.com>
2022-01-22 19:09:10 +0200

    Add bubble sort.

---
+++ src/com/company/BubbleSort.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import java.util.Arrays;
+
+/*
+ *  Algorithm complexity: O(n^2)
+ *
+ *  The algorithm consists of repeated passes through the sorted array.
+ *  For each pass, the elements are sequentially compared in pairs and,
+ *  if the order in the pair is incorrect, the elements are permuted.
+ */
+
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        int[] arr = {64, -34, 25, 12, 22, 11, 90, -2};
+        System.out.println(Arrays.toString(arr));
+        bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++){
+            for (int j = 0; j < n-i-1; j++){
+                if (arr[j] > arr[j+1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+
+                }
+            }
+        }
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,13 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int[] arr = {1, 12, -4, 5, 16, 1, 0, -13};
+        BubbleSort.bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
     }
 }
